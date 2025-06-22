474aaa1c4cac9a0e9be84cc78f9fed7b05221f99
thotenn <josenriqueee@gmail.com>
2025-06-22 19:50:34 -0300

    delete/unnecessary-files

--- packages/shared-components/dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { PhoenixProvider, usePhoenix } from './components/PhoenixProvider';
-export { AuthGuard } from './components/AuthGuard';
-export { PermissionGuard } from './components/PermissionGuard';
-export { useChannel } from './hooks/useChannel';
-export { usePermissions } from './hooks/usePermissions';
-export { filterRoutesByPermissions, createProtectedRoute } from './utils/routes';
-export type { RouteConfig } from './utils/routes';
--- packages/shared-components/dist/utils/routes.d.ts
+++
@@ -1,9 +0,0 @@
-export interface RouteConfig {
-    path: string;
-    permissions?: string[];
-    roles?: string[];
-    requireAuth?: boolean;
-    children?: RouteConfig[];
-}
-export declare const filterRoutesByPermissions: (routes: RouteConfig[], userPermissions: string[], userRoles: string[]) => RouteConfig[];
-export declare const createProtectedRoute: (path: string, options?: Partial<RouteConfig>) => RouteConfig;
